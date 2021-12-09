0a00516168a7d57cbb715e16425e83cc50a340ff
ADRIANGDENIA <47852361+ADRIANGDENIA@users.noreply.github.com>
2021-12-09 17:35:06 +0100

    Paquete tipos

---
+++ Trabajo_FPGA/Sources/tipos_esp.vhd
@@ -0,0 +1,8 @@
+package tipos_esp is
+generic ( MAX_ROUND : natrual := 99);
+type natural_vector is array (0 to MAX_ROUND-1) of natural;
+
+end tipos_esp;
+
+package body tipos_esp is
+end tipos_esp;
